3df8b7849b6233ede7c5c29157e681875923f728
Abdullah Al Masud <abdullahalmasud01737906772@gmail.com>
2023-09-23 18:09:39 +0600

    first commit

---
+++ App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { SafeAreaView, Text, View } from 'react-native';
+import { Button, ButtonGroup, CheckBox } from '@rneui/themed';
+import { Icon } from '@rneui/base';
+import { Header } from '@rneui/themed';
+import LinearGradient from 'react-native-linear-gradient';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Image } from '@rneui/themed';
+
+const App = () => {
+
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView>
+        <View style={{ width: '100%', height: '100%', backgroundColor: 'gray' }}>
+          <Text>sdlkfjdslk</Text>
+        </View>
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+};
+
+export default App;
